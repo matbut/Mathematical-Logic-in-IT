e2485b822fb7a23422632172901a6ca0499af5e9
matbut <butamateusz@gmail.com>
2017-06-04 12:47:20 +0200

    Update Zad 3 implementacja.hs

--- Zad 3 implementacja.hs
+++ Zad 3 implementacja.hs
@@ -6,44 +6,44 @@ data C = String
 data D = Char
 
 impl_rozdz :: (A -> B) -> (A -> C) -> A -> B -> C
-proof_impl_rozdz h i j k = i j
+impl_rozdz h i j k = i j
 
 impl_komp :: (A -> B) -> (B -> C) -> A -> C
-proof_impl_komp h i j = i (h j)
+impl_komp h i j = i (h j)
 
 impl_perm :: (A -> B -> C) -> B -> A -> C
-proof_impl_perm h i j = h j i
+impl_perm h i j = h j i
 
 impl_conj :: A -> B -> (A,B)
-proof_impl_conj h i = (h,i)
+impl_conj h i = (h,i)
 
 conj_elim_l :: (A,B) -> A
-proof_conj_elim_l (i,j) = i 
+conj_elim_l (i,j) = i 
 
 disj_intro_l :: A -> Either A B
-proof_disj_intro_l h = Left h
+disj_intro_l h = Left h
 
 rozl_elim :: Either A B -> (A -> C) -> (B -> C) -> C
-proof_rozl_elim (Left h) i j = i h
-proof_rozl_elim (Right h) i j = j h
+rozl_elim (Left h) i j = i h
+rozl_elim (Right h) i j = j h
 
 diamencik :: (A -> B) -> (A -> C) -> (B -> C -> D) -> A -> D
-proof_diamencik h i j k = j (h k)(i k)
+diamencik h i j k = j (h k)(i k)
 
 slaby_peirce :: ((((A -> B) -> A) -> A) -> B) -> B
-proof_slaby_peirce h = h (\i -> i (\j -> h (\k -> j)))
+slaby_peirce h = h (\i -> i (\j -> h (\k -> j)))
 
 rozl_impl_rozdz :: ((Either A B) -> C) -> ((A -> C),(B -> C))
-proof_rozl_impl_rozdz h = ((\i -> h (Left i)),(\j -> h (Right j)))
+rozl_impl_rozdz h = ((\i -> h (Left i)),(\j -> h (Right j)))
 
 rozl_impl_rozdz_odw :: ((A -> C),(B -> C)) -> Either A B -> C
-proof_rozl_impl_rozdz_odw (h,i) (Left j) = h j
-proof_rozl_impl_rozdz_odw (h,i) (Right j) = i j
+rozl_impl_rozdz_odw (h,i) (Left j) = h j
+rozl_impl_rozdz_odw (h,i) (Right j) = i j
 
 ccurry :: ((A,B) -> C) -> A -> B -> C
-proof_ccurry h i j = h (i,j)
+ccurry h i j = h (i,j)
 
 unccurry :: (A -> B -> C) -> (A,B) -> C
-proof_unccurry h (i,j) = h i j
+unccurry h (i,j) = h i j
 
 main = print "Zad3 Mateusz Buta"
